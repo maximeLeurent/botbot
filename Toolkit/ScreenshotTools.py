15936e1b0309e65f4cd66d5738d4bd0000a1c8ea
AlbertDuval <mollier.fr@gmail.com>
2018-05-17 23:46:28 +0200

    Add Docstring to the takeScreenshot function

--- Toolkit/ScreenshotTools.py
+++ Toolkit/ScreenshotTools.py
@@ -2,6 +2,36 @@ import os
 import win32gui, win32ui, win32con
 
 def takeScreenshot(windowName, width, height, left, top, keepScreen=True, getText=False):
+    """
+        This function allow to take screenshot of a specific window designate
+        by is name, you must also specify, the starting point (left, top) and
+        the size (width, height) of the zone you want to extractself.
+        Then you can decide to keep or not the generated file and choose to try
+        to evaluate a text from this file with pytesseract prebuild method.
+
+        :param windowName: The name of the window
+        :type windowName: string
+        :param width: The width of the zone you want to capture
+        :type width: int
+        :param height: The height of the zone you want to capture
+        :type height: int
+        :param left: The left position of starting point on top left corner
+        :type left: int
+        :param top: The top position of starting point on top left corner
+        :type top: int
+        :param keepScreen: Boolean to keep the generated file
+        :type keepScreen: boolean
+        :param getText: Boolean to try to getText from the capture
+        :type getText: boolean
+
+        :return: Eventually the text extract from the capture
+        :rtype: string
+
+        :Example:
+        >>> takeScreenshot("Blizzard Battle.net", 300, 20, 370, 700, getText=True)
+        Gameis mmllng.
+        (Expected "Game is running")
+    """
     # grab a handle to the main desktop window
     #hdesktop = win32gui.GetDesktopWindow()
     hdesktop = win32gui.FindWindow(None, windowName)
